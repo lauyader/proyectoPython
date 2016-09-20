59ed7035be464da36be60ee265e5c3fe073f4c1d
Luis Americo Auyadermont Muñoz <lauyader2011@gmail.com>
2016-09-20 17:06:12 -0400

    Rutinas para detección de objetos y colores

--- aprendiendo/funciones.py
+++ aprendiendo/funciones.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 def calculo():
-	print "Convierte medidas inglesas a sistema métrico"
-        millas = input("Cuántas millas?:")
-        pies = input("Y cuantos pies?:")
-	metros = 24*millas+pies
-	print "Longitud", metros, "metros" 
+    print ("Convierte medidas inglesas a sistema métrico")
+    millas = input("Cuántas millas?:")
+    pies = input("Y cuantos pies?:")
+    metros = 24*millas+pies
+    print ("Longitud", metros, "metros")
 
 def suma():
-	print "Funcion de suma"
+    print ("Funcion de suma")
 
 calculo()
+
+def prueva():
